a7305b6a3234fbf4a5236b2599aea0b6f5233f6b
Mr Moon <mrmoon@una.edu>
2022-09-08 13:30:43 -0500

    updated code to add line breaks

--- clean.sh
+++ clean.sh
@@ -5,18 +5,13 @@ OUTPUT=""
 for dir in $FILES
 do
   if [ -f $dir/.log/ssh ] && [ -f $dir/.log/bashhistory ]; then
-    OUTPUT+=$(cat $dir/.log/ssh)
-    OUTPUT+=$'\n'
-    OUTPUT+=$(diff -u $dir/.log/bashhistory $dir/.bash_history | grep -E "^\+")
+    OUTPUT+="${dir#/*/}"$'\n'$(cat $dir/.log/ssh)$'\n'$(diff -u $dir/.log/bashhistory $dir/.bash_history | grep -E "^\+")$'\n\n'
     rm -f $dir/.log/bashhistory $dir/.log/ssh
   fi
 done
 
 if [ -f /root/.log/ssh ] && [ -f /root/.log/bashhistory ]; then
-  OUTPUT+=$'\n'
-  OUTPUT+=$(cat /root/.log/ssh)
-  OUTPUT+=$'\n'
-  OUTPUT+=$(diff -u /root/.log/bashhistory /root/.bash_history | grep -E "^\+")
+  OUTPUT+=$'\n'"root"$'\n'$(cat /root/.log/ssh)$'\n'$(diff -u /root/.log/bashhistory /root/.bash_history | grep -E "^\+")$'\n\n'
   rm -f /root/.log/bashhistory /root/.log/ssh
 fi
 
